aa2ef43d06f001a4362398296943c0410e05a271
lutherbrian <lutherbrian@gmail.com>
2020-09-04 13:09:25 +0100

    dfdfd

---
+++ FballTableTop/Assets/Scripts/MovePanelScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovePanelScript : MonoBehaviour
+{
+
+    private GameObject PanelOut;
+    public GameObject EndPositionPOUT;
+    private float speed = 0.8f;
+
+    public float PanelInDelay = 0.0f;
+
+    public GameObject panelIn;
+
+    public GameObject StartPosition;
+    private Vector3 EndPositionPIN;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PanelOut = this.gameObject;
+        EndPositionPIN = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MovePanelOut()
+    {
+        PanelOut.LeanMoveLocal (EndPositionPOUT.transform.position, speed).setEaseInOutBack();
+
+        StartCoroutine(AnimatePanelIn(PanelInDelay));
+    }
+
+
+
+
+
+    IEnumerator AnimatePanelIn(float Time)
+    
+    {
+        yield return new WaitForSeconds(Time);
+
+        panelIn.transform.localPosition = StartPosition.transform.position;
+
+
+
+        panelIn.LeanMoveLocal(EndPositionPIN, speed).setEaseInOutBack();
+
+
+    }
+}
---
+++ FballTableTop/Assets/Scripts/ScrollText.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScrollText : MonoBehaviour
+
+{
+
+    public TextMeshProUGUI TextMEshProComponenet;
+    public float ScrollSpeed = 10;
+
+    private TextMeshProUGUI m_cloneTextObject;
+
+    private RectTransform m_textRectTransform;
+    private string sourceText;
+    private string tempText;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_textRectTransform = TextMEshProComponenet.GetComponent<RectTransform>();
+        m_cloneTextObject = Instantiate(TextMEshProComponenet) as TextMeshProUGUI;
+        RectTransform clonerectTransform = m_cloneTextObject.GetComponent<RectTransform>();
+        clonerectTransform.SetParent(m_textRectTransform);
+        clonerectTransform.localScale = new Vector3(1, 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Start()
+    {
+        float width = TextMEshProComponenet.preferredWidth;
+        Vector3 startPosition = m_textRectTransform.position;
+
+        float scrollPosition = 0;
+
+        while (true)
+        {
+            if (TextMEshProComponenet.transform.hasChanged)
+            {
+                width = TextMEshProComponenet.preferredWidth;
+                m_cloneTextObject.text = TextMEshProComponenet.text;
+            }
+            
+            m_textRectTransform.position = new Vector3(-scrollPosition % width, startPosition.y, startPosition.z);
+
+            scrollPosition += ScrollSpeed * 20 * Time.deltaTime;
+
+            yield return null;
+
+        }
+
+
+    }
+}
