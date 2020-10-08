373fd94b98ab9fb501b216b666eb70aaa7b1a6f5
lutherbrian <lutherbrian@gmail.com>
2020-10-08 23:17:55 +0100

    league

--- FballTableTop/Assets/Scripts/AddLeagueTable.cs
+++ FballTableTop/Assets/Scripts/AddLeagueTable.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class AddLeagueTable : MonoBehaviour
 {
+
+    public GameObject contentpanel;
+    public GameObject Leaguestrip;
+    private Vector3 OpenPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        OpenPosition = new Vector3(0, 0, 0);
+        addleaugue();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void addleaugue()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            GameObject newgameObject = (GameObject)Instantiate(Leaguestrip);
+
+            newgameObject.transform.SetParent(contentpanel.transform, false);
+        }
+
     }
 }
