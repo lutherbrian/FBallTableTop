37ea98804b3f1f49bddcb4c1f372610d2ef7a2cd
lutherbrian <lutherbrian@gmail.com>
2020-10-26 23:37:40 +0000

    dfdf

--- FballTableTop/Assets/Scripts/GameManagement/ChooseYourTeam.cs
+++ FballTableTop/Assets/Scripts/GameManagement/ChooseYourTeam.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 using TMPro;
 
 public class ChooseYourTeam : MonoBehaviour
@@ -10,15 +11,31 @@ public class ChooseYourTeam : MonoBehaviour
     public GameObject gameManagment;
     public TMP_Dropdown dropdown;
     public TMP_InputField inputfield;
-    
+
+    public TMP_InputField Team1;
+    public TMP_InputField Team2;
+    public TMP_InputField Team3;
+    public TMP_InputField Team4;
+    public TMP_InputField Team5;
+    public TMP_InputField Team6;
+    public TMP_InputField Team7;
+    public TMP_InputField Team8;
+    public TMP_InputField Team9;
+    public TMP_InputField Team10;
+    public TMP_InputField Team11;
+    public TMP_InputField Team12;
+    public TMP_InputField Team13;
+
     private GameManagment gamemanagementScript;
     private int PlayerNUM = 0;
     private GameObject Team;
     // Start is called before the first frame update
     void Start()
     {
-        
-        gamemanagementScript = gameManagment.GetComponent<GameManagment>();
+        List<string> m_DropOptions = new List<string> { Team1.text, Team2.text, Team3.text, Team4.text, Team5.text, Team6.text, Team7.text, Team8.text, Team9.text, Team10.text, Team11.text, Team12.text, Team13.text };
+        dropdown.ClearOptions();
+        dropdown.AddOptions(m_DropOptions);
+
     }
 
     // Update is called once per frame
@@ -27,13 +44,24 @@ public class ChooseYourTeam : MonoBehaviour
       
     }
 
+    public void addDropOptions()
+
+    {
+        List<string> m_DropOptions = new List<string> { Team1.text, Team2.text, Team3.text, Team4.text, Team5.text, Team6.text, Team7.text, Team8.text, Team9.text, Team10.text, Team11.text, Team12.text, Team13.text };
+        dropdown.ClearOptions();
+        dropdown.AddOptions(m_DropOptions);
+    }
+
     public void AddPlayer()
     {
         
                 Team = GameObject.Find("Team"+PlayerNUM);
                 Team.GetComponent<Team>().playerName = inputfield.text;
                 Team.GetComponent<Team>().TeamName = dropdown.options[dropdown.value].text;
-       // Team.gameObject.name = (inputfield.text + "'s Team");
+                dropdown.options.RemoveAt(dropdown.value);
+                dropdown.value = dropdown.value + 1;
+                dropdown.value = 0;
+
                 inputfield.text = "";
 
                 PlayerNUM++;
