7b90973e58cfe4f6a82eff5241b1d56a7db070b5
lutherbrian <lutherbrian@gmail.com>
2020-11-04 16:52:51 +0000

    as

---
+++ FballTableTop/Assets/Scripts/UI/OpenCloseSettings.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCloseSettings : MonoBehaviour
+{
+
+    private bool open = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OpenClose()
+
+    {
+
+        if (open == false)
+        {
+            LeanTween.rotate(this.gameObject, new Vector3(0, 0, -90), .75f).setEaseInOutBack();
+            open = true;
+        }
+
+        else
+        {
+
+            LeanTween.rotate(this.gameObject, new Vector3(0, 0, 90), .75f).setEaseInBack();
+            open = false;
+        }
+    }
+}
